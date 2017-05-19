943aa751f9729e04b83e818706e4e3485ce82006
Matthew <matthew.walinga@gmail.com>
2017-05-18 23:32:47 -0400

    Add implementation for list2tree

--- Braun Trees/C1.ml
+++ Braun Trees/C1.ml
@@ -12,20 +12,17 @@ let rec merge lst1 lst2 = match (lst1, lst2) with
 
 (* Algorithm description: recursively convert the left and right trees into
     lists and merge the lists as we go *)
+(* Time efficiency: O(n*2)?? *)
 let rec tree2list = function
   | Empty -> []
   | Node (e, left, right) -> e :: merge (tree2list left) (tree2list right)
 
-let rec mergeTrees b1 b2 = match (b1, b2) with
-  | (Empty, r) -> r
-  | (l, Empty) -> l
-  | () -> Node (hd, tl, getright tl)
+(* The following is a naive implementation of list-to-tree *)
+(* Time efficiency: O(n*log n) *)
+let rec extend e = function
+  | Empty -> Node (e, empty, empty)
+  | Node (elem, left, right) -> Node (e, extend elem right, left)
 
-(* This function is supposed to split a list into two parts *)
-let rec break hd tl = match (hd, tl) with
-  | _ -> tl 
-    
 let rec list2tree = function
   | [] -> Empty
-  | [e] -> Node (e, Empty, Empty)
-  | a :: tl -> Node (a, break (list2tree tl))
+  | hd :: tl -> extend e (list2tree tl)
