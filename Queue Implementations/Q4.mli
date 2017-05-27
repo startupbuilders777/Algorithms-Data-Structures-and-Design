f5a71cfd784d3e73e2d93921f550eb2648e034d3
Harman Singh <harman.j.singh@hotmail.com>
2017-05-27 02:44:24 -0400

    Start 2 Stream implementation of Queue

---
+++ Queue Implementations/Q4.mli
@@ -0,0 +1,27 @@
+(*
+The four fields in a queue tuple represent the length of the front segment, 
+the front segment, the length of the rear segment, and the rear segment, respectively.
+*)
+
+module type StreamType = sig
+  type 'a stream_cell = Nil | Cons of 'a * 'a stream
+  and 'a stream = 'a stream_cell Lazy.t
+ 
+  val (++) : 'a stream -> 'a stream -> 'a stream  (* stream append *)
+  val rev : 'a stream -> 'a stream
+end
+ 
+module Stream : StreamType
+ 
+module type Queue = sig
+  type 'a queue = int * 'a Stream.stream * int * 'a Stream.stream
+ 
+  val empty : 'a queue
+  val is_empty : 'a queue -> bool
+ 
+  val snoc : 'a -> 'a queue -> 'a queue
+  val first : 'a queue -> 'a option
+  val rest : 'a queue -> 'a queue option
+end
+ 
+module TSQueue : Queue
