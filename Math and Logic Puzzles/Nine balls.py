37a0c0b5187ef0185739ecb1ba9fd3e27473ea0d
Harman Singh <harman.j.singh@hotmail.com>
2017-08-19 00:26:09 -0400

    Doing werk

---
+++ Math and Logic Puzzles/Nine balls.py
@@ -0,0 +1,20 @@
+'''
+
+You have 9 balls, 8 are the same weight and one is heavier. You are given a balance which tells you only whther the
+left side or the right side is havier. Find the heavy ball in just 2 uses of the scale.
+
+Put 3 balls on one side and 3 balls on the other side and leave out 3 balls.
+Same weight then the 3 balls left out contain heavy ball.
+otherwise the 3 balls that are heavier contain the heavy ball.
+
+
+Repeat process for 3 balls with one heavy.
+
+weigh 2. and leave out one. If they are equal weight then ball left out is heavy.
+Otherwise the scale will indicate the heavy ball.
+
+
+
+
+
+'''
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ Cover vertical line case in intersection
 Complete powerSet
 Do second part of magicIndex
 Do the bit version of numberSwapper
+
+complete shortest super sequence
+
+Sieve of Eratoshenes Algo -> Write this algo out for prime number generation
