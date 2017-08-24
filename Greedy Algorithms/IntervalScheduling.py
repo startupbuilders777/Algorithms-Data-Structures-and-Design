2b0ae99a40fd0173786b055c4c30f75b5efac4b6
Harman Singh <harman.j.singh@hotmail.com>
2017-08-24 15:36:14 -0400

    fook

---
+++ Greedy Algorithms/IntervalScheduling.py
@@ -0,0 +1,11 @@
+'''
+
+Input: n intervals [s1, f1], [s2, f2], ... , [sn, fn]
+Output: a maximum set of disjoint intervals
+
+'''
+
+intervals = {
+    (1,2), (1, 7), (2, 6), (4, 5), (5, 9), (9,11),(6,7), (7,9), (4,7),
+}
+
